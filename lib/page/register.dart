7289ea8633d94ad8cb159fc2bc74cf45cbf981c4
NguyenDucTinNew <93468105+HuyLieu1603@users.noreply.github.com>
2024-03-20 18:12:33 +0700

    update

--- lib/page/register.dart
+++ lib/page/register.dart
@@ -25,18 +25,71 @@ class _RegisterWidgetState extends State<RegisterWidget> {
       ),
       body: Container(
         color: Colors.white,
-        child: const Column(
+        padding: const EdgeInsets.all(16),
+        child: Column(
           children: [
-            Text(
-              "Bạn đã sẵn sàng để xem chưa?",
-              style: titleStyle,
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
+            const SizedBox(
               height: 16,
             ),
-            Text(
-                "Nhập Email để tạo hoặc kích hoạt lại tư cách thành viên của bạn"),
+            const Text(
+              "Bạn đã sẵn sàng để xem chưa?",
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const Text(
+              "Nhập Email để tạo hoặc kích hoạt lại tư cách thành viên của bạn",
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(
+                    vertical: 40.0, horizontal: 10.0),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(11.0),
+                  borderSide: const BorderSide(
+                    color: Colors.green,
+                  ),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(11.0),
+                  borderSide: const BorderSide(
+                    width: 5.0,
+                    color: Colors.green,
+                  ),
+                ),
+                labelText: "Email",
+              ),
+            ),
+            const SizedBox(
+              height: 18,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(198, 198, 10, 10),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 10,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  minimumSize: (const Size(370, 40))),
+              child: const Text(
+                "Bắt đầu",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
           ],
         ),
       ),
